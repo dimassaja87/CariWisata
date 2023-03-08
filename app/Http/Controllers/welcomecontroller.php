f8499a3f2faba18784ad4918fe34a04120164313
Gmbs <gmbsstok@gmail.com>
2023-03-08 14:47:39 +0700

    qwertyuio

--- app/Http/Controllers/welcomecontroller.php
+++ app/Http/Controllers/welcomecontroller.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Kota;
+use App\Models\Wisata;
 
 class welcomecontroller extends Controller
 {
      public function welcome()
      {
         $data = kota::all();
-            return view('welcome',compact('data'));
+        $data1 = Wisata::all();
+            return view('welcome',compact('data','data1'));
      }
 }
