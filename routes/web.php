f42531521b5b82eb6522ddc6d77f912a905e7181
dimassaja87 <122768607+dimassaja87@users.noreply.github.com>
2023-03-08 10:15:03 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\RatingController;
 use App\Http\Controllers\KontakController;
 use App\Http\Controllers\WisataController;
 use App\Http\Controllers\welcomecontroller;
+use App\Models\Wisata;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,21 +29,19 @@ use App\Http\Controllers\welcomecontroller;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('welcome', [welcomecontroller::class, 'welcome'])->name('welcome');
-
-
+Route::get('/welcome', [welcomecontroller::class, 'welcome'])->name('welcome');
 
 Route::get('/u', function () {
     return view('user.welcomeuser');
 });
 
 Route::get('/adminn', function () {
-    $jumlahdestinasi = Destinasi::count();
+    $jumlahwisata = Wisata::count();
     $jumlahuser = User::count();
     $jumlahkota = Kota::count();
     // $jumlahulasan = Ulasan::count();
     // $jumlahulasan = Ulasan::count();
-    return view('admin.welcomeadmin', compact('jumlahdestinasi', 'jumlahuser', 'jumlahkota'));
+    return view('admin.welcomeadmin', compact('jumlahwisata', 'jumlahuser', 'jumlahkota'));
 });
 
 Route::get('/destinasi', function () {
