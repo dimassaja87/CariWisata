0760333a3bb8ce821b89e343416fecbbdd74d1cb
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2023-03-09 13:46:09 +0700

    profile
    capekk

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function profile()
+    {
+        $profile=Profile::all();
+        return view('profile.profil',['profile'=>$profile]);
+    }
+    public function updateprofile(Request $request)
+    {
+        $data = Profile::create([
+            'nama' => $request->nama,
+            'foto' => $request->foto,
+            'email' => $request->email,
+            'keterangan' => $request->keterangan,
+        ]);
+        if ($request->hasFile('foto')) {
+            $request->file('foto')->move('fotoprofile/', $request->file('foto')->getClientOriginalName());
+            $data->foto = $request->file('foto')->getClientOriginalName();
+            $data->save();
+
+            return back()->with('success', 'Data Behasil Ditambahkan!');
+        }
+    }
+}
